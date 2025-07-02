19b915246cc3223b5abe4aca0021637ac804aa63
Nathan Rothenbaum <102261062+cheepsahoy@users.noreply.github.com>
2025-07-02 11:48:10 -0500

    added templates for react components, installed icon library

---
+++ etAL_client/etal_client/src/components/Header.jsx
@@ -0,0 +1,11 @@
+import { ChartNetwork } from "lucide-react";
+function Header() {
+  return (
+    <>
+      <ChartNetwork />
+      <h className="header">etAL</h>
+      <p>Join the Conversation</p>
+    </>
+  );
+}
+export default Header;
