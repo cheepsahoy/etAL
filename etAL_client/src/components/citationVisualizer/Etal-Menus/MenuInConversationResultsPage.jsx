f71a562eeb24e3e31f0bc93dd25f30119dac1e33
Nathan Rothenbaum <102261062+cheepsahoy@users.noreply.github.com>
2026-08-03 19:49:46 -0500

    Fix class case on MenuInConversationResultsPage

--- etAL_client/src/components/citationVisualizer/Etal-Menus/MenuInConversationResultsPage.jsx
+++ etAL_client/src/components/citationVisualizer/Etal-Menus/MenuInConversationResultsPage.jsx
@@ -1,62 +1,57 @@
-import MenuInConversationCard from "./MenuInConversationCard";
+import MenuInConversationCard from './MenuInConversationCard'
 
-function MenuInConversationResultsPage({ pageNumber, setPageNumber, subData }) {
-  if (subData.length === 0) {
-    return <p>Waiting on data...</p>;
-  } else {
-    const lastPageIndex = subData.length - 1;
-    const relevantPage = subData[Math.min(pageNumber, lastPageIndex)];
+function MenuInConversationResultsPage({pageNumber, setPageNumber, subData}) {
+    if (subData.length === 0) {
+        return <p>Waiting on data...</p>
+    } else {
+        const lastPageIndex = subData.length - 1
+        const relevantPage = subData[Math.min(pageNumber, lastPageIndex)]
 
-    function backWardsHandler() {
-      if (pageNumber === 0) {
-        return;
-      } else {
-        setPageNumber((prev) => Math.max(prev - 1, 0));
-      }
+        function backWardsHandler() {
+            if (pageNumber === 0) {
+                return
+            } else {
+                setPageNumber(prev => Math.max(prev - 1, 0))
+            }
+        }
+
+        function forwardsHandler() {
+            setPageNumber(prev => Math.min(prev + 1, lastPageIndex))
+        }
+
+        return (
+            <div
+                className="inConversationResults"
+                style={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                }}>
+                <ul>
+                    {relevantPage.map(article => {
+                        const uniqueID = article.id
+                        return (
+                            <li key={uniqueID}>
+                                <MenuInConversationCard data={article} />
+                            </li>
+                        )
+                    })}
+                </ul>
+                <div
+                    className="menuButtons"
+                    style={{
+                        display: 'flex',
+                        flexDirection: 'row',
+                    }}>
+                    <button onClick={backWardsHandler} disabled={pageNumber === 0}>
+                        Page Back
+                    </button>
+                    <button onClick={forwardsHandler} disabled={pageNumber >= lastPageIndex}>
+                        Page Forward
+                    </button>
+                </div>
+            </div>
+        )
     }
-
-    function forwardsHandler() {
-      setPageNumber((prev) => Math.min(prev + 1, lastPageIndex));
-    }
-
-    return (
-      <div
-        className="inConversationResults"
-        style={{
-          display: "flex",
-          flexDirection: "column",
-        }}
-      >
-        <ul>
-          {relevantPage.map((article) => {
-            const uniqueID = article.id;
-            return (
-              <li key={uniqueID}>
-                <MenuInConversationCard data={article} />
-              </li>
-            );
-          })}
-        </ul>
-        <div
-          className="menuBUttons"
-          style={{
-            display: "flex",
-            flexDirection: "row",
-          }}
-        >
-          <button onClick={backWardsHandler} disabled={pageNumber === 0}>
-            Page Back
-          </button>
-          <button
-            onClick={forwardsHandler}
-            disabled={pageNumber >= lastPageIndex}
-          >
-            Page Forward
-          </button>
-        </div>
-      </div>
-    );
-  }
 }
 
-export default MenuInConversationResultsPage;
+export default MenuInConversationResultsPage
